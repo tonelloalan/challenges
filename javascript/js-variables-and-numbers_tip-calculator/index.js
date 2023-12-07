658ab378472470641ef322e8ecbef2caeebdc2d6
ALAN TONELLO <alantb2@gmail.com>
2023-12-07 14:47:49 +0100

    update tip value

--- javascript/js-variables-and-numbers_tip-calculator/index.js
+++ javascript/js-variables-and-numbers_tip-calculator/index.js
@@ -1,5 +1,5 @@
 const mealPrice = 15.0;
-const tipPercentage = 0.01;
+const tipPercentage = 0.1; // 10% tip
 const tipAmount = mealPrice * tipPercentage;
 const totalCost = mealPrice + tipAmount;
 console.log("The total cost of your bill is: " + totalCost + " euros.");

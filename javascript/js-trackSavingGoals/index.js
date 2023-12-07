efa1131bc4ed92bc922ee25f304a175a35c71196
ALAN TONELLO <alantb2@gmail.com>
2023-12-07 16:03:45 +0100

    initialized last variable and printed it

--- javascript/js-trackSavingGoals/index.js
+++ javascript/js-trackSavingGoals/index.js
@@ -1,3 +1,7 @@
 let startingBalance = 1000;
 let monthlyContribution = 200;
 let months = 24;
+let totalSavings = startingBalance + monthlyContribution * months;
+console.log(
+  "After " + months + " months, your total saving is: " + totalSavings + " €."
+);

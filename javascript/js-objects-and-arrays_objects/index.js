d6c2138207f689fcdc8aa00ecf3a03e1e91f2b9f
ALAN TONELLO <alantb2@gmail.com>
2023-12-11 11:36:23 +0100

    update date and age of Alex

--- javascript/js-objects-and-arrays_objects/index.js
+++ javascript/js-objects-and-arrays_objects/index.js
@@ -17,8 +17,8 @@ const personAlex = {
   age: 24,
 };
 
-const nameOfAlex = "Change me";
-const ageOfAlex = "Change me";
+const nameOfAlex = personAlex.name;
+const ageOfAlex = personAlex["age"];
 
 // EXERCISE 3
 // Change the value of name to "Alex" and the value of age to "35" by updating the object personToChange: person.existingProperty = newValue.

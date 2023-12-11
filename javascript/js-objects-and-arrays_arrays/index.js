71ecf94fa31b9a6fa706f55c8533074809cb72b2
ALAN TONELLO <alantb2@gmail.com>
2023-12-11 11:04:23 +0100

    modified nestedArray

--- javascript/js-objects-and-arrays_arrays/index.js
+++ javascript/js-objects-and-arrays_arrays/index.js
@@ -8,7 +8,7 @@ const exampleArray = [34, "string"];
 // EXERCISE 2
 // Nest an array inside array nestedArray. After that nestedArray should contain an array as an element.
 
-const nestedArray = ["example", 10, true];
+const nestedArray = ["example", 10, true, ["a", "b"]];
 
 // EXERCISE 3
 // Change the value of firstNumber to equal the first value of the numbers array using bracket notation.

5e909468139903e294b99d124ffb25c118047d2c
ALAN TONELLO <alantb2@gmail.com>
2023-12-11 11:38:23 +0100

    update name and age of person

--- javascript/js-objects-and-arrays_objects/index.js
+++ javascript/js-objects-and-arrays_objects/index.js
@@ -28,6 +28,9 @@ const personToChange = {
   age: 24,
 };
 
+personToChange.name = "Alex";
+personToChange.age = 35;
+
 // EXERCISE 4
 // Add the properties age "5" and breed "husky" to the object petPluto: pet.newProperty = newValue.
 

9337f817b90ec1a4ded3ec3e1823daef72a8db00
ALAN TONELLO <alantb2@gmail.com>
2023-12-11 11:56:53 +0100

    display 3rd person's age

--- javascript/js-objects-and-arrays_objects-and-arrays/index.js
+++ javascript/js-objects-and-arrays_objects-and-arrays/index.js
@@ -60,7 +60,7 @@ const homersOtherFriends = people[0]["other friends"];
 // EXERCISE 3
 // Change the value of the variable ageOfThirdPerson to equal the age of the third person.
 
-const ageOfThirdPerson = "change me";
+const ageOfThirdPerson = people[2].age;
 
 // EXERCISE 4
 // Change the value of the variable mothersNameOfSecondPerson to equal the mothers name of the second person.

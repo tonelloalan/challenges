ed6703f4913a7df491d936ef26380f0987ecd449
ALAN TONELLO <alantb2@gmail.com>
2023-12-12 11:04:44 +0100

    create function for exercise separators

--- javascript/js-functions_functions-with-parameters/js/index.js
+++ javascript/js-functions_functions-with-parameters/js/index.js
@@ -1,7 +1,6 @@
 console.clear();
 
-console.log("-------------");
-console.log("Exercise 1:");
+logSeparator(1);
 /*
 1: The function below is not executed. Can you figure out why? Find the bug and fix it.
 */
@@ -14,8 +13,7 @@ function logText() {
   console.log("This text shoudl appear now.");
 }
 
-console.log("-------------");
-console.log("Exercise 2:");
+logSeparator(2);
 /*
 2: Look at the functions below. They are almost identical. Can you find a way to generalize them into a new function with an input parameter? Replace the function calls below with your new function.
 */
@@ -45,14 +43,15 @@ function greet(name) {
 greet("Alan");
 // --^-- write your code here --^--
 
-console.log("-------------");
-console.log("Exercise 3:");
-/*
+logSeparator(3); /*
 3: We log a little separation text for each subtask. This is repetitive code, so lets write a function 'logSeparator' which takes the number of the exercise as a parameter and logs the respective separator. Then use this function to replace the existing separator logs in this javascript file.
 
 Note: Separation text means the text that is logged into the console before each exercise. For example: "-------------" and "Exercise 1:" are the separation texts for the first exercise.
 */
 
 // --v-- write your code here --v--
-
+function logSeparator(exerciseNumber) {
+  console.log("-------------");
+  console.log("Exercise " + exerciseNumber + ":");
+}
 // --^-- write your code here --^--

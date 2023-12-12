e5427831ea199d94f24e0cba9a507461e32a74fe
ALAN TONELLO <alantb2@gmail.com>
2023-12-12 11:31:37 +0100

    declared function

--- javascript/js-functions_functions-with-parameters-extra/js/index.js
+++ javascript/js-functions_functions-with-parameters-extra/js/index.js
@@ -39,5 +39,10 @@ printCircumference(6);
 
 // Uncomment the following function calls and implement the function printRectangleArea
 
-// printRectangleArea(5, 7);
-// printRectangleArea(3, 4);
+printRectangleArea(5, 7);
+printRectangleArea(3, 4);
+
+function printRectangleArea(width, length) {
+  const area = width * length;
+  console.log("The area of the rectangle is: " + area);
+}

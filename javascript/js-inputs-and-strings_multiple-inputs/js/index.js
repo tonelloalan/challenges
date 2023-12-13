bed084a795815dc08be326d18951f20e3ae9c4e4
ALAN TONELLO <alantb2@gmail.com>
2023-12-13 17:00:12 +0100

    complete first challenge

--- javascript/js-inputs-and-strings_multiple-inputs/js/index.js
+++ javascript/js-inputs-and-strings_multiple-inputs/js/index.js
@@ -14,3 +14,8 @@ Implement the following functionality:
 const firstInput = document.querySelector("[data-js=first-input]");
 const secondInput = document.querySelector("[data-js=second-input]");
 const button = document.querySelector("[data-js=button]");
+
+button.addEventListener("click", () => {
+  const firstValue = firstInput.value;
+  secondInput.value = firstValue;
+});

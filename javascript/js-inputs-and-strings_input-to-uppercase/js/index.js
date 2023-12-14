69d9472c712fc09fd307afe81857f93080af124b
ALAN TONELLO <alantb2@gmail.com>
2023-12-14 15:59:08 +0100

    assign inout element to variable

--- javascript/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ javascript/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -11,6 +11,7 @@ Hint: Access the value by using `.value` on the input element
 
 */
 
+const input = document.querySelector("input");
 const button = document.querySelector('[data-js="button-uppercase"]');
 
 button.addEventListener("click", () => {

e6f0cd8a4ff703dd455c04ad9fb29b121dc5c636
ALAN TONELLO <alantb2@gmail.com>
2023-12-15 11:39:38 +0100

    create variable with assigned paragraph, add class and append variable

--- javascript/js-createelement_social-media-post/js/index.js
+++ javascript/js-createelement_social-media-post/js/index.js
@@ -6,11 +6,16 @@ function handleLikeButtonClick(event) {
 }
 
 const likeButton = document.querySelector('[data-js="like-button"]');
+
 likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
 
-const newPost = document.createElement("section"); // assigned the post section to  a variable
+const newPost = document.createElement("section"); // assigned the post container section to  a variable
 newPost.classList.add("post"); // added post class (style) to the new variable from  a class list
-document.append(newPost); // append new post with the class
+document.body.append(newPost); // append new post with the class
+
+const newPostContent = document.createElement("p"); // assigned the post content section to  a variable
+newPostContent.classList.add("post__content"); // added post content class (style) to the new variable from  a class list
+newPostContent.textContent = "Your new post content here."; // assign new text for the post content

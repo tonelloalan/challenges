e47600252b9073912f4efeb5a1045450dbfcca25
ALAN TONELLO <alantb2@gmail.com>
2023-12-16 09:32:21 +0100

    add further notes

--- javascript/js-forms_handle-form-submit/js/index.js
+++ javascript/js-forms_handle-form-submit/js/index.js
@@ -3,12 +3,12 @@ console.clear();
 const form = document.querySelector('[data-js="form"]'); // targeted the form item and assigned its value to a variable
 
 form.addEventListener("submit", (event) => {
+  event.preventDefault(); // prevents the form submition to trigger the standard action from the browser, which is refreshing the page and printing the form values on the navigator
+
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
 
   console.log(data);
-
-  event.preventDefault();
 
   const firstName = document.getElementById("first-name");
 

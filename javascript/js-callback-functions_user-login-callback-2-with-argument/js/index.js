5bbcea87437397ad93fdb69bfe986f2670ab69b2
ALAN TONELLO <alantb2@gmail.com>
2023-12-19 16:44:40 +0100

    write function with  requested keyword

--- javascript/js-callback-functions_user-login-callback-2-with-argument/js/index.js
+++ javascript/js-callback-functions_user-login-callback-2-with-argument/js/index.js
@@ -5,3 +5,8 @@ function handleUserLogin(onSuccess) {
 }
 
 // The exercise starts here!
+
+// Write a function with the "function" keyword named "showWelcomeMessage"
+function showWelcomeMessage(userName) {
+  console.log(`Welcome ${userName}! You are logged in now.`);
+}

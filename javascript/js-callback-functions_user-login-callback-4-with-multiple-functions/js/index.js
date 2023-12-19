1d6eca365f40df72879c755f171dd4acf0fdfdeb
ALAN TONELLO <alantb2@gmail.com>
2023-12-19 17:45:44 +0100

    create 2nd function

--- javascript/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
+++ javascript/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
@@ -12,3 +12,7 @@ function handleUserLogin(onSuccess, onError, userName) {
 function showWelcomeMessage(userName, userRole) {
   console.log(`Welcome ${userName}! You are logged in now as ${userRole}`);
 }
+
+function showErrorMessage(errorMessage) {
+  console.log(`Login error: ${errorMessage} `);
+}

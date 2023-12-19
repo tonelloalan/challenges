534d88bd9d1e82853d2f3a5641ae2f31f0e30e29
ALAN TONELLO <alantb2@gmail.com>
2023-12-19 16:45:45 +0100

    call function

--- javascript/js-callback-functions_user-login-callback-2-with-argument/js/index.js
+++ javascript/js-callback-functions_user-login-callback-2-with-argument/js/index.js
@@ -10,3 +10,6 @@ function handleUserLogin(onSuccess) {
 function showWelcomeMessage(userName) {
   console.log(`Welcome ${userName}! You are logged in now.`);
 }
+
+// call "handleUserLogin" and pass your callback function as the first argument
+handleUserLogin(showWelcomeMessage, "Jane Doe");

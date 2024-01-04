43adba87d0268e62662f113fa5b350c07d795261
ALAN TONELLO <alantb2@gmail.com>
2024-01-04 16:09:41 +0100

    move App function to its own folder, imported functions Header, Form and CardList, and imported App function to index.js

--- javascript/js-structure_quiz-app/index.js
+++ javascript/js-structure_quiz-app/index.js
@@ -3,6 +3,7 @@ import Card from "./components/Card/Card.js";
 import CardList from "./components/CardList/CardList.js";
 import Form from "./components/Form/Form.js";
 import Header from "./components/Header/Header.js";
+import App from "./components/App/App.js";
 export { cards };
 
 // Store the cards in a global state
@@ -33,14 +34,8 @@ const cards = [
 // moved function Bookmark to its own folder
 // moved function Card to its own folder
 // moved function CardList to its own folder
-
-function App() {
-  const app = document.createElement("main");
-  app.classList.add("app");
-  app.append(Header(), Form(), CardList());
-
-  return app;
-}
+// moved function Form to its own folder
+// moved function Header to its own folder
 
 // The render function renders the app to the DOM
 function render() {

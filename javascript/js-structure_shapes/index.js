b0d5351397f67b361f34b4cf5bfbac5808f8e97b
ALAN TONELLO <alantb2@gmail.com>
2024-01-04 12:49:06 +0100

    wrap creation of circle element in new function Circle

--- javascript/js-structure_shapes/index.js
+++ javascript/js-structure_shapes/index.js
@@ -5,12 +5,15 @@ console.clear();
 
 const root = document.getElementById("root");
 
-const circle = document.createElement("div");
-circle.classList.add("circle");
-circle.addEventListener("click", () => {
-  // circle.style.backgroundColor = "#ccc";
-  circle.style.backgroundColor = `${getRandomColor()}`; // use function getRandomColor inside all three event listeners
-});
+// wrap the creation of the circle element including the addEventListener into a separate function starting with a capital letter, e.g. Circle
+function Circle() {
+  const circle = document.createElement("div");
+  circle.classList.add("circle");
+  circle.addEventListener("click", () => {
+    // circle.style.backgroundColor = "#ccc";
+    circle.style.backgroundColor = `${getRandomColor()}`; // use function getRandomColor inside all three event listeners
+  });
+}
 
 const square = document.createElement("div");
 square.classList.add("square");

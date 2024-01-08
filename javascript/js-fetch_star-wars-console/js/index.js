c21cb55fc6f8a0f996e49bb2de1e3b8246c83490
ALAN TONELLO <alantb2@gmail.com>
2024-01-08 15:17:25 +0100

    mark function with async

--- javascript/js-fetch_star-wars-console/js/index.js
+++ javascript/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,14 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {} // 1. mark the function with the async keyword, because we want to use await inside the function.
 
 fetchData();
+
+/* 
+async function fetchData() {
+  const response = await fetch("/url/to/something");
+  const data = await response.json();
+  return data;
+} 
+*/

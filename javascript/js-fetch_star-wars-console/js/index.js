5be7a58d127561eeefef526cfda1293626521b49
ALAN TONELLO <alantb2@gmail.com>
2024-01-08 15:33:32 +0100

    log different values of the data object

--- javascript/js-fetch_star-wars-console/js/index.js
+++ javascript/js-fetch_star-wars-console/js/index.js
@@ -7,8 +7,11 @@ async function fetchData() {
   const response = await fetch(url); // declare a variable named response. It stores the Response object (previsouly defined variable "url") that is returned by fetch.
 
   const data = await response.json(); // Once this promise resolves (the network request is finished), we call the .json method on the response variable. This function returns another Promise.
-  console.log(data);
+  console.log(data); // log entire data object
+  console.log(data.count); // log different values of the data object
+  console.log(data.results); // log different values of the data object
 
   return data; // This second promise resolves with the actual data (payload) converted from JSON (a formatted string) to a JavaScript value or object. This result is stored in the variable named data.
 }
+
 fetchData();

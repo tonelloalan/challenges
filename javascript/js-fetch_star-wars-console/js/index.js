2359f8739d53a567d4203bc2244a2c505672e817
ALAN TONELLO <alantb2@gmail.com>
2024-01-08 15:27:52 +0100

    log entire data object

--- javascript/js-fetch_star-wars-console/js/index.js
+++ javascript/js-fetch_star-wars-console/js/index.js
@@ -5,16 +5,10 @@ const url = "https://swapi.dev/api/people";
 async function fetchData() {
   // 1. mark the function with the async keyword, because we want to use await inside the function.
   const response = await fetch(url); // declare a variable named response. It stores the Response object (previsouly defined variable "url") that is returned by fetch.
-  console.log(response);
+
   const data = await response.json(); // Once this promise resolves (the network request is finished), we call the .json method on the response variable. This function returns another Promise.
+  console.log(data);
+
   return data; // This second promise resolves with the actual data (payload) converted from JSON (a formatted string) to a JavaScript value or object. This result is stored in the variable named data.
 }
 fetchData();
-
-/* 
-async function fetchData() {
-  const response = await fetch("/url/to/something");
-  const data = await response.json();
-  return data;
-} 
-*/

2694404ce2db2b9c5d1919fa0c24cdef4b2c8c81
ALAN TONELLO <alantb2@gmail.com>
2024-01-09 15:28:05 +0100

    code 3rd test

--- javascript/js-unit-testing_get-animal/index.js
+++ javascript/js-unit-testing_get-animal/index.js
@@ -3,26 +3,10 @@ console.clear();
 export function getAnimal(animalPluralName) {
   if (animalPluralName === "cats") {
     return "I totally love cats!";
+  } else if (!animalPluralName) {
+    return `I do not like animals at all!`;
   } else {
     return `I like ${animalPluralName}!`;
   }
 }
 console.log(getAnimal("dogs"));
-
-/*  
-import { getAnimal } from "./index.js";
-
-test("getAnimals() returns a string 'I totally love cats!' if called with the argument 'cats'.", () => {
-  const result = getAnimal("cats");
-  expect(result).toBe("I totally love cats!");
-});
-test("getAnimals() returns a string 'I like [animalPluralName]!' if called with a string argument.", () => {
-  const result = getAnimal("dogs");
-  expect(result).toBe("I like dogs!");
-});
-test("getAnimals() returns a string 'I do not like animals at all!' if called without arguments.", () => {
-  const result = getAnimal();
-  expect(result).toBe("I do not like animals at all!");
-});
-
-*/

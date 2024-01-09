8199686342d1d5e52c789384fecad76cbdae99fd
ALAN TONELLO <alantb2@gmail.com>
2024-01-09 15:54:39 +0100

    complete add() function test

--- javascript/js-unit-testing_math-functions/index.test.js
+++ javascript/js-unit-testing_math-functions/index.test.js
@@ -2,7 +2,15 @@
 import { add, subtract, multiply, divide } from "./index.js"; // import function
 
 // test add() function
-test("adds the numbers 1 and 2 correctly", () => {
-  const result = add(1, 2);
-  expect(result).toBe(3);
+test("returns 5 if called with add(2, 3)", () => {
+  const result = add(2, 3);
+  expect(result).toBe(5);
+});
+test("returns a negative value if the greater argument is negative", () => {
+  const result = add(2, -3);
+  expect(result).toBeLessThan(0);
+});
+test("returns a value close to 0.3 if called with add(0.1, 0.2)", () => {
+  const result = add(0.1, 0.2);
+  expect(result).toBeCloseTo(0.3);
 });

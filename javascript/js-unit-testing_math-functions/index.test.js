9cfabd31f3180218291f9a15407f2b5fb0b3e65f
ALAN TONELLO <alantb2@gmail.com>
2024-01-09 15:45:14 +0100

    write code for add() function test

--- javascript/js-unit-testing_math-functions/index.test.js
+++ javascript/js-unit-testing_math-functions/index.test.js
@@ -1,1 +1,8 @@
-import { add, subtract, multiply, divide } from "./index.js";
+// create index.test.js
+import { add, subtract, multiply, divide } from "./index.js"; // import function
+
+// test add() function
+test("adds the numbers 1 and 2 correctly", () => {
+  const result = add(1, 2);
+  expect(result).toBe(3);
+});

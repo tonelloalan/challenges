4b1dffcbab16edc0f0fde1002522695413023b4f
ALAN TONELLO <alantb2@gmail.com>
2024-01-15 16:44:08 +0100

    create flexible button

--- react/react-props/react-props_button/src/App.js
+++ react/react-props/react-props_button/src/App.js
@@ -2,5 +2,34 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  // return <h1>Replace me with your component!</h1>;
+  return (
+    <Button
+      padding="8px"
+      borderRadius="15px"
+      text="Click me!"
+      color="red"
+      bgColor="yellow"
+    />
+  );
+}
+
+function Button({ padding, borderRadius, bgColor, color, disabled, text }) {
+  return (
+    <button
+      style={{
+        padding: padding,
+        borderRadius: borderRadius,
+        backgroundColor: bgColor,
+        color: color,
+        display: disabled,
+      }}
+    >
+      {text}
+    </button>
+  );
+}
+
+{
+  /* <h1 style="color: red; font-size: 40px;">Hello World</h1> */
 }

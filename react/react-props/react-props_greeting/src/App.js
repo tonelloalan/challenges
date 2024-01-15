4a5bad5dec3e4c615e01c666a5a87fb068e4d40e
ALAN TONELLO <alantb2@gmail.com>
2024-01-15 15:03:30 +0100

    implement ternary operator to conditionally render greeting message

--- react/react-props/react-props_greeting/src/App.js
+++ react/react-props/react-props_greeting/src/App.js
@@ -2,9 +2,10 @@ import "./styles.css";
 
 export default function App() {
   // return <h1>Replace me with your component!</h1>;
-  return <Greeting name="Alan" />;
+  return <Greeting name="Marcell" />;
 }
 
 function Greeting({ name }) {
-  return <h2>Hello, {name}!</h2>;
+  // return <h2>Hello, {name}!</h2>;
+  return <h2>{name === "Marcell" ? "Hello, Coach!" : `Hello, ${name}!`}</h2>; // implemented ternary operator to conditionally render greeting message
 }

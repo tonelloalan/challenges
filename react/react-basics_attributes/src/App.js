4a5f55c307d9a6b35f3d7c179ea6629faa823562
ALAN TONELLO <alantb2@gmail.com>
2024-01-15 11:40:19 +0100

    final commit

--- react/react-basics_attributes/src/App.js
+++ react/react-basics_attributes/src/App.js
@@ -2,5 +2,23 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">This is my article title</h2>
+      <label htmlFor="my-input">
+        Input Label:
+        <input id="my-input" placeholder="Type here..." />
+      </label>
+      <a
+        className="article__link"
+        href="https://en.wikipedia.org/wiki/React_(software)"
+      >
+        React Wikipedia Article
+      </a>
+    </article>
+  );
 }

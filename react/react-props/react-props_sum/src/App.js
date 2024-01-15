7c27cd446b8482292e095a358554e5db813db499
ALAN TONELLO <alantb2@gmail.com>
2024-01-15 16:05:28 +0100

    creat Sum() component and called it inside Add()

--- react/react-props/react-props_sum/src/App.js
+++ react/react-props/react-props_sum/src/App.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  // return <h1>Replace me with your component!</h1>;
+  return <Sum valueA={2} valueB={4} />;
+}
+
+function Sum({ valueA, valueB }) {
+  let result = valueA + valueB;
+  // return <h2>{`${valueA} + ${valueB} = ${result}`}</h2>; // option 1
+  return (
+    <h2>
+      {valueA} + {valueB} = {result}
+    </h2>
+  );
 }

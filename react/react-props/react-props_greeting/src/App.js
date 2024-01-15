0f2b52d4a4239553eb0ebd1f29fd98630a522afc
ALAN TONELLO <alantb2@gmail.com>
2024-01-15 14:50:23 +0100

    update App() function with Greeting() function

--- react/react-props/react-props_greeting/src/App.js
+++ react/react-props/react-props_greeting/src/App.js
@@ -1,7 +1,10 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  // return <h1>Replace me with your component!</h1>;
+  return <Greeting name="Alan" />;
 }
 
-function Greeting({ name }) {}
+function Greeting({ name }) {
+  return <h2>Hello, {name}!</h2>;
+}

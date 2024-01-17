1406d8fd4c3a3a60f6c0c143ea6259cbc12441d5
ALAN TONELLO <alantb2@gmail.com>
2024-01-17 16:16:27 +0100

    used map() method in the USERS array to render individual cards for each user and highlight their tag admin in case present

--- react/react-with-array/react-with-arrays_users/src/components/Tag.js
+++ react/react-with-array/react-with-arrays_users/src/components/Tag.js
@@ -1,5 +1,8 @@
 import "./Tag.css";
 
+// receive an additional class called 'Tag--highlight' if the tag prop equals the string 'admin'
 export default function Tag({ tag }) {
-  return <li className="tag">{tag}</li>;
+  return (
+    <li className={`tag ${tag === "admin" ? "tag--highlight" : ""}`}>{tag}</li>
+  );
 }

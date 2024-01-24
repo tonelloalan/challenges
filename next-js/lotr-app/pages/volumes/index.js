cd37f6adaae383a68d258f8121d64f3b3a2261da
ALAN TONELLO <alantb2@gmail.com>
2024-01-24 16:35:04 +0100

    create ul with volumes titles, using dynamic link text and page address

--- next-js/lotr-app/pages/volumes/index.js
+++ next-js/lotr-app/pages/volumes/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Head from "next/head";
 import { introduction } from "../../resources/lib/data.js";
+import { volumes } from "../../resources/lib/data.js";
 
 export default function Volumes() {
   return (
@@ -10,6 +11,18 @@ export default function Volumes() {
       </Head>
       <h1>Lord of the Rings</h1>
       <p>{introduction}</p>
+      <h2>All Volumes</h2>
+      <ul>
+        <li>
+          <Link href={`/volumes/${volumes[0].slug}`}>{volumes[0].title}</Link>
+        </li>
+        <li>
+          <Link href={`/volumes/${volumes[1].slug}`}>{volumes[1].title}</Link>
+        </li>
+        <li>
+          <Link href={`/volumes/${volumes[0].slug}`}>{volumes[2].title}</Link>
+        </li>
+      </ul>
     </>
   );
 }

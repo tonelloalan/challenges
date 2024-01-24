3e110e728a34d935817ed900997be0b401aca5d8
ALAN TONELLO <alantb2@gmail.com>
2024-01-24 16:27:35 +0100

    import introduction

--- next-js/lotr-app/pages/index.js
+++ next-js/lotr-app/pages/index.js
@@ -1,10 +1,16 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Lord of the Rings App</h1>
-      <Link href="/volumes">Volumes</Link>
-    </div>
+    <>
+      <Head>
+        <title>My Lord of The Rings App</title>
+      </Head>
+      <div>
+        <h1>Lord of the Rings App</h1>
+        <Link href="/volumes">Volumes</Link>
+      </div>
+    </>
   );
 }
--- next-js/lotr-app/pages/volumes/index.js
+++ next-js/lotr-app/pages/volumes/index.js
@@ -1,1 +1,15 @@
-export default function Volumes() {}
+import Link from "next/link";
+import Head from "next/head";
+import { introduction } from "../../resources/lib/data.js";
+
+export default function Volumes() {
+  return (
+    <>
+      <Head>
+        <title>My LOTR App: Volumes</title>
+      </Head>
+      <h1>Lord of the Rings</h1>
+      <p>{introduction}</p>
+    </>
+  );
+}

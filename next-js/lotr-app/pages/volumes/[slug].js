b6fa1e7882890334543ef4e2cf6cb532a47aa43d
ALAN TONELLO <alantb2@gmail.com>
2024-01-25 11:10:16 +0100

    recreate Volume component which returns the same elements as the static version of the component

--- next-js/lotr-app/pages/volumes/[slug].js
+++ next-js/lotr-app/pages/volumes/[slug].js
@@ -2,11 +2,14 @@ import Link from "next/link.js";
 import { volumes } from "../../resources/lib/data.js";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import Head from "next/head.js";
 
 export default function Volume() {
   const router = useRouter();
   const { slug } = router.query;
   const currentVolume = volumes.find((volume) => volume.slug === slug);
+  console.log(currentVolume);
+
   if (!currentVolume) {
     return (
       <>
@@ -14,53 +17,61 @@ export default function Volume() {
         <Link href="/volumes">← Back to all Volumes</Link>
       </>
     );
+  } else {
+    const { title, description } = currentVolume;
+
+    return (
+      <>
+        <Head>
+          <title>{currentVolume.title}</title>
+        </Head>
+        {/* <Link href="/volumes">← All Volumes</Link> */}
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <ul>
+          {currentVolume.books.map((book, index) => (
+            <li key={index}>
+              {/* the key={index} attribute is used to provide a unique key to each <li> element, which is necessary for React to efficiently update the list when it changes. */}
+              <strong>{book.ordinal}:</strong> {book.title}
+            </li>
+          ))}
+        </ul>
+        <Image
+          src={currentVolume.cover}
+          alt={`${currentVolume.title}`}
+          width={140}
+          height={230}
+        />
+        <br />
+        <Link href={`/volumes/${title.slug}`}>Next</Link>
+      </>
+    );
   }
-
-  const { title, description, year } = currentVolume;
-
-  return (
-    <>
-      <Link href="/volumes">← All Volumes</Link>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <ul>
-        {/* maps over the array of books in the Fellowship of the Ring volume. */}
-        {{ title }.books.map((book, index) => (
-          <li key={index}>
-            {/* the key={index} attribute is used to provide a unique key to each <li> element, which is necessary for React to efficiently update the list when it changes. */}
-            <strong>{book.ordinal}:</strong> {book.title}
-          </li>
-        ))}
-      </ul>
-      <Image
-        src={{ title }.cover}
-        alt={`${{ title }.title}`}
-        width={140}
-        height={230}
-      />
-      <br />
-      <Link href={`/volumes/${{ title }.slug}`}>Next</Link>
-    </>
-  );
 }
 
-// const currentMovie = movies.find((movie) => movie.slug === slug);
+// export default function Movie() {
+//     const router = useRouter()
+//     const {slug} = router.query
 
-//   if (!currentMovie) {
-//     return (<><h1>This movie doesnt exist</h1>;
-// <Link href="/movies">← Back to all movies</Link>
-//     </>)
+//     console.log(router);
+
+//     const currentMovie = movies.find((movie) => movie.slug === slug);
+
+//     if (!currentMovie) {
+//       return (<><h1>This movie doesnt exist</h1>;
+//   <Link href="/movies">← Back to all movies</Link>
+//       </>)
+//     }
+
+//     const { title, description, year } = currentMovie;
+
+//     return (
+//       <>
+//         <h1>
+//           {title} <small>{year}</small>
+//         </h1>
+//         <p>{description}</p>
+//         <Link href="/movies">← Back to all movies</Link>
+//       </>
+//     );
 //   }
-
-//   const { title, description, year } = currentMovie;
-
-//   return (
-//     <>
-//       <h1>
-//         {title} <small>{year}</small>
-//       </h1>
-//       <p>{description}</p>
-//       <Link href="/movies">← Back to all movies</Link>
-//     </>
-//   );
-// }
--- next-js/lotr-app/pages/volumes/index.js
+++ next-js/lotr-app/pages/volumes/index.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import Head from "next/head";
-import { introduction } from "../../resources/lib/data.js";
-import { volumes } from "../../resources/lib/data.js";
+import { introduction, volumes } from "../../resources/lib/data.js";
+import { useRouter } from "next/router";
 
 export default function Volumes() {
+  const router = useRouter();
+
   return (
     <>
       <Head>
@@ -13,15 +15,13 @@ export default function Volumes() {
       <p>{introduction}</p>
       <h2>All Volumes</h2>
       <ul>
-        <li>
-          <Link href={`/volumes/${volumes[0].slug}`}>{volumes[0].title}</Link>
-        </li>
-        <li>
-          <Link href={`/volumes/${volumes[1].slug}`}>{volumes[1].title}</Link>
-        </li>
-        <li>
-          <Link href={`/volumes/${volumes[2].slug}`}>{volumes[2].title}</Link>
-        </li>
+        {volumes.map((volume) => {
+          return (
+            <li key={volume.id}>
+              <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+            </li>
+          );
+        })}
       </ul>
     </>
   );

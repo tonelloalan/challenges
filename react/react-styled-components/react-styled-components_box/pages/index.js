3187548e447fa9d65eb4ad4d60ea4aed176eec71
ALAN TONELLO <alantb2@gmail.com>
2024-01-26 11:31:17 +0100

    refactor code

--- react/react-styled-components/react-styled-components_box/pages/index.js
+++ react/react-styled-components/react-styled-components_box/pages/index.js
@@ -1,14 +1,16 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
 import BoxWithStyledComponents from "@/components/BoxWithStyledComponents.js";
 import styled from "styled-components";
+import StyledBox from "@/components/BoxWithStyledComponents.js";
 
+// replace div with StyledContainer to apply display flex to boxes
 export default function HomePage() {
   return (
     <StyledContainer>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
-      <BoxWithStyledComponents />
-      <BoxWithStyledComponents isBlack />
+      <StyledBox />
+      <StyledBox $isBlack />
     </StyledContainer>
   );
 }

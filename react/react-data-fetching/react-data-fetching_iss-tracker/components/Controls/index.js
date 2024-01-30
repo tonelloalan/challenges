a1ca6b4636162f14c691203acb89c2157db10908
ALAN TONELLO <alantb2@gmail.com>
2024-01-30 11:35:36 +0100

    Refetch on button click

--- react/react-data-fetching/react-data-fetching_iss-tracker/components/Controls/index.js
+++ react/react-data-fetching/react-data-fetching_iss-tracker/components/Controls/index.js
@@ -4,14 +4,14 @@ import {
   ControlsDisplay,
 } from "./Controls.styled";
 
-// export default function Controls({ longitude, latitude, onRefresh }) {
-export default function Controls({ longitude, latitude }) {
+export default function Controls({ longitude, latitude, onRefresh }) {
   return (
     <ControlsContainer>
       <ControlsDisplay>Lat: {longitude.toFixed(5)}</ControlsDisplay>
       <ControlsDisplay>Long: {latitude.toFixed(5)}</ControlsDisplay>
-      {/* <ControlsButton type="button" onClick={onRefresh}> */}
-      <ControlsButton type="button">Refresh</ControlsButton>
+      <ControlsButton type="button" onClick={onRefresh}>
+        Refresh
+      </ControlsButton>
     </ControlsContainer>
   );
 }

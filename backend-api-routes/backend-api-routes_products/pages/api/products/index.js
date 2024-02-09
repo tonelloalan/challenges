ef48c275d9be5fb9bc96c9492925c6ef085bf376
ALAN TONELLO <alantb2@gmail.com>
2024-02-09 10:54:10 +0100

    create an api route which returns all products

---
+++ backend-api-routes/backend-api-routes_products/pages/api/products/index.js
@@ -0,0 +1,6 @@
+import { getAllProducts } from "@/services/productServices"; // import getAllProducts
+
+// write a handler function which responds with...
+export default function handler(req, res) {
+  res.status(200).json(getAllProducts()); // ... a 200 status code and with the return value of getAllProducts(), parsed with the .json() method
+}

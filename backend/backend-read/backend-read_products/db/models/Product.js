937d5e21a6d9540d0e314a98f3a048f1cc9f58ee
ALAN TONELLO <alantb2@gmail.com>
2024-02-12 12:43:12 +0100

    finish exercise without bonus

---
+++ backend/backend-read/backend-read_products/db/models/Product.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  name: { type: String },
+  description: { type: String },
+  price: { type: Number },
+  currency: { type: String },
+});
+
+const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
+
+export default Product;
--- backend/backend-read/backend-read_products/pages/api/products/[id].js
+++ backend/backend-read/backend-read_products/pages/api/products/[id].js
@@ -1,9 +1,9 @@
-import { products } from "../../../lib/products";
+import Product from "@/db/models/Product";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
   const { id } = request.query;
 
-  const product = products.find((product) => product.id === id);
+  const product = await Product.findById(id);
 
   if (!product) {
     return response.status(404).json({ status: "Not Found" });

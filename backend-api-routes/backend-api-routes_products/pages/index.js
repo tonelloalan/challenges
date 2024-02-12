b615fab1631c5fd24d309058cc1eadc192db1e9a
ALAN TONELLO <alantb2@gmail.com>
2024-02-12 10:53:08 +0100

    modified changes

--- backend-api-routes/backend-api-routes_products/pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Welcome to Next.js API Routes!</h1>
-    </div>
-  );
-}
--- react/react-global-state/react-global-state_lights/pages/index.js
+++ react/react-global-state/react-global-state_lights/pages/index.js
@@ -1,6 +1,7 @@
 import Link from "../components/Link";
 
-export default function HomePage() {
+// added two props: lights and handleLightToggle
+export default function HomePage(lights, handleLightToggle) {
   return (
     <div>
       <h1>Home</h1>
